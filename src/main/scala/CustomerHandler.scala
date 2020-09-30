1b4a2f8d6578bcebacded2c3c8f2d5150b9905fd
Davide Icardi <davide.icardi@gmail.com>
2020-09-30 22:58:00 +0200

    topology driver

--- src/main/scala/CustomerHandler.scala
+++ src/main/scala/CustomerHandler.scala
@@ -1,30 +1,38 @@
 import java.util.Properties
 
-import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.scala.Serdes._
+import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.{StreamsConfig, Topology}
-import Serdes._
+import com.davideicardi.kaa.SchemaRegistry
+import org.apache.kafka.common.serialization.Serde
 
 class CustomerHandler(
                      val bootstrapServers: String,
-                     val commandsTopic: String,
-                     val snapshotTopic: String,
-                       /*, eventsTopic: String*/
+                     val schemaRegistry: SchemaRegistry,
+                     val commandsTopic: String = "customers.commands",
+                     val eventsTopic: String = "customers.events",
+                     val snapshotTopic: String = "customers.snapshots",
                      val applicationId: String = "CustomerHandler",
                      ) {
   val properties = new Properties()
   properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId)
   properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
 
-  def create(): Topology = {
+  implicit val commandValueSerde: Serde[Customer.Command] =
+    new com.davideicardi.kaa.kafka.GenericSerde[Customer.Command](schemaRegistry)
+
+  def createTopology(): Topology = {
     val streamBuilder = new StreamsBuilder
-    val stream: KStream[String, String] =
+    val commands: KStream[String, Customer.Command] =
       streamBuilder.stream(commandsTopic)
 
-    stream
-      .mapValues((_, v) => v.toUpperCase())
+    commands
       .to(snapshotTopic)
+
+    commands
+      .to(eventsTopic)
 
     streamBuilder.build()
   }
--- src/test/scala/CustomerHandlerSpec.scala
+++ src/test/scala/CustomerHandlerSpec.scala
@@ -1,33 +1,36 @@
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.apache.kafka.streams.TopologyTestDriver
+import com.davideicardi.kaa.test.TestSchemaRegistry
+import org.apache.kafka.common.serialization.Serde
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.apache.kafka.streams.scala.Serdes._
 
 import scala.jdk.CollectionConverters._
 
 class CustomerHandlerSpec extends AnyFunSpec with Matchers {
+  private val schemaRegistry = new TestSchemaRegistry
 
-  private val stringSer = new StringSerializer()
-  private val stringDes = new StringDeserializer()
+  implicit val commandValueSerde: Serde[Customer.Command] =
+    new com.davideicardi.kaa.kafka.GenericSerde[Customer.Command](schemaRegistry)
 
   describe("when sending a CommandCreate") {
     it("generate events") {
       val target = new CustomerHandler(
         "dummy:9999",
+        schemaRegistry,
         "commands",
         "snapshots",
       )
-      val topology = target.create()
-      val driver = new TopologyTestDriver(topology, target.properties)
+      val topology = target.createTopology()
+      val driver = new ScalaTopologyTestDriver(topology, target.properties)
 
-      val inputTopic = driver.createInputTopic(target.commandsTopic, stringSer, stringSer)
-      val outputTopic = driver.createOutputTopic(target.snapshotTopic, stringDes, stringDes)
+      val inputTopic = driver.createInputTopic[String, Customer.Command](target.commandsTopic)
+      val outputTopic = driver.createOutputTopic[String, Customer.Command](target.snapshotTopic)
 
-      inputTopic.pipeInput("key1", "value1")
+      inputTopic.pipeInput("key1", Customer.CommandCreate("code1", "name1"))
 
       val results = outputTopic.readKeyValuesToMap().asScala
 
-      results should be(Map("key1" -> "VALUE1"))
+      results should be(Map("key1" -> Customer.CommandCreate("code1", "name1")))
     }
   }
 }
