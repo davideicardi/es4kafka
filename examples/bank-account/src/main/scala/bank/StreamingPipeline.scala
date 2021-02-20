df84354964ee1c784ee5ac60bd4f3b0edd19783e
Davide Icardi <davide.icardi@gmail.com>
2021-02-20 02:18:33 +0100

    refactoring

--- examples/bank-account/src/main/scala/bank/StreamingPipeline.scala
+++ examples/bank-account/src/main/scala/bank/StreamingPipeline.scala
@@ -3,6 +3,7 @@ package bank
 import com.davideicardi.kaa.SchemaRegistry
 import es4kafka.Inject
 import es4kafka.streaming._
+import es4kafka.streaming.EventSourcingDsl._
 import es4kafka.serialization.CommonAvroSerdes._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.ImplicitConversions._
@@ -23,23 +24,19 @@ class StreamingPipeline @Inject()(
     // commands
     val operationsStream = streamBuilder.stream[String, Operation](Config.topicOperations)
 
-    val movementsStream = operationsStream.transformValues(
-      new CommandHandler.Supplier[String, Operation, Movement, Account](
-        Config.storeAccounts,
-        { request =>
-          val operation = request.command
-          val account = request.state.getOrElse(Account(0))
-          val newAccount = account.copy(balance = account.balance + operation.amount)
-          if (newAccount.balance >= 0) {
-            val movement = Movement(operation.amount)
-            new CommandHandler.Response(movement, Some(newAccount))
-          } else {
-            val movement = Movement(0, error = "insufficient funds")
-            new CommandHandler.Response(movement)
-          }
-        },
-      )
+    val movementsStream = operationsStream.eventSourcing[Movement, Account](
+      Config.storeAccounts,
+      { (_, operation, state) =>
+        val account = state.getOrElse(Account(0))
+        val newAccount = account.copy(balance = account.balance + operation.amount)
+        if (newAccount.balance >= 0) {
+          (Seq(Movement(operation.amount)), Some(newAccount))
+        } else {
+          (Seq(Movement(0, error = "insufficient funds")), state)
+        }
+      },
     )
+
     movementsStream.to(Config.topicMovements)
 
     streamBuilder
